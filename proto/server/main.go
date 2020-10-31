54960a4d8bff795b7cdef5ae9d19b23257abf336
Hoda Mohammadi <mohamhoda@gmail.com>
2020-10-31 13:17:06 -0400

    create horoscope server

---
+++ proto/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"github.com/hodamohammadi/grpc-example/horoscope"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := horoscope.NewServer{}
+
+	grpcServer := grpc.NewServer()
+
+	horoscope.RegisterHoroscopeServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
