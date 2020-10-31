0110fa95248f49c82f0c6929b0f4ed02bc91f433
Hoda Mohammadi <mohamhoda@gmail.com>
2020-10-31 13:38:20 -0400

    use proto

--- server/horoscope.go
+++ server/horoscope.go
@@ -1,3 +1,4 @@
+// Package horoscope handles Horoscope RPC calls
 package horoscope
 
 import (
@@ -8,14 +9,12 @@ import (
 	pb "github.com/hodamohammadi/grpc-example/proto/horoscope"
 )
 
-type Server struct {
+type Server struct {}
 
-}
-
-func (s *Server) GetHoroscope(ctx context.Context, in *horoscope.GetHoroscopeRequest) (*horoscope.GetHoroscopeResponse, error) {
+func (s *Server) GetHoroscope(ctx context.Context, in *pb.GetHoroscopeRequest) (*pb.GetHoroscopeResponse, error) {
 	log.Printf("Receive request from client: %s", in)
 	s := fmt.Sprintf("A new cycle is beginning for you in which you may find yourself throwing away old beliefs and mental processes, %s.", in.Sign.String()) +
 	 "Out with the old and in with the new. This time of housecleaning is extremely important, for you will find that the same tired " +
 	 "old speech that you've been working on is suddenly defunct. Pull your resources together and construct a new platform that makes you proud."
-	return &horoscope.GetHoroscopeResponse{Text: s}, nil
+	return &pb.GetHoroscopeResponse{Text: s}, nil
 }
